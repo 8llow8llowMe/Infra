248cf438a90239b99f00651fcb17e063f5ac3aca
신동근 <tlsehdrms124@naver.com>
2025-04-16 17:05:26 +0900

    [INFRA] fix: 도커 엔트리 포인트 초기화 쉘 스크립트 수정

--- Jenkins/docker-entrypoint-init.sh
+++ Jenkins/docker-entrypoint-init.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+LOG_FILE="/var/log/jenkins-entrypoint.log"
+touch "$LOG_FILE"
+
+echo "[$(date)] docker.sock의 GID를 동기화 시작합니다." | tee -a "$LOG_FILE"
+
 # docker.sock의 GID 동기화
 SOCK="/var/run/docker.sock"
 if [ -S "$SOCK" ]; then
     GID=$(stat -c '%g' "$SOCK")
     groupadd -for -g "$GID" docker
     usermod -aG docker jenkins
-    echo "[INFO] Added jenkins user to docker group (GID=$GID)"
+    echo "[$(date)] [INFO] jenkins 사용자를 docker 그룹에 추가했습니다. (GID=$GID)" | tee -a "$LOG_FILE"
+else
+    echo "[$(date)] [WARN] docker.sock이 존재하지 않습니다. (/var/run/docker.sock)" | tee -a "$LOG_FILE"
 fi
+
+# ENTRYPOINT 작동 확인용 테스트 파일 생성
+touch /tmp/jenkins-entrypoint-test
+echo "[$(date)] [INFO] ENTRYPOINT 실행 완료" | tee -a "$LOG_FILE"
 
 # 기존 entrypoint로 넘어감
 exec /usr/bin/tini -- /usr/local/bin/jenkins.sh "$@"
