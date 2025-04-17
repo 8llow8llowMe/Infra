84a2052b0eb3da19dbd6f901ae7381559d28ff68
신동근 <tlsehdrms124@naver.com>
2025-04-17 11:07:43 +0900

    [INFRA] fix: 도커 엔트리포인터 설정 관련 쉘 스크립트 수정

--- Jenkins/docker-entrypoint-init.sh
+++ Jenkins/docker-entrypoint-init.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-LOG_FILE="/var/log/jenkins-entrypoint.log"
+LOG_FILE="/tmp/jenkins-entrypoint.log"
 touch "$LOG_FILE"
 
 echo "[$(date)] docker.sock의 GID를 동기화 시작합니다." | tee -a "$LOG_FILE"
 
-# docker.sock의 GID 동기화
 SOCK="/var/run/docker.sock"
 if [ -S "$SOCK" ]; then
     GID=$(stat -c '%g' "$SOCK")
@@ -16,9 +15,7 @@ else
     echo "[$(date)] [WARN] docker.sock이 존재하지 않습니다. (/var/run/docker.sock)" | tee -a "$LOG_FILE"
 fi
 
-# ENTRYPOINT 작동 확인용 테스트 파일 생성
 touch /tmp/jenkins-entrypoint-test
 echo "[$(date)] [INFO] ENTRYPOINT 실행 완료" | tee -a "$LOG_FILE"
 
-# 기존 entrypoint로 넘어감
 exec /usr/bin/tini -- /usr/local/bin/jenkins.sh "$@"
