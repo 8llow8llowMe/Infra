c746946e5547f82e4ab2ff11a3f81260a611b0ae
신동근 <tlsehdrms124@naver.com>
2025-05-11 22:45:41 +0900

    [INFRA] fix: 도커 엔트리포인트 수정 (젠킨스 관련)

--- Jenkins/docker-entrypoint-init.sh
+++ Jenkins/docker-entrypoint-init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 LOG_FILE="/tmp/jenkins-entrypoint.log"
@@ -14,11 +13,14 @@ if [ -S "$SOCK" ]; then
     usermod -aG docker jenkins
     echo "[$(date)] [INFO] jenkins 사용자를 docker 그룹에 추가했습니다. (GID=$GID)" | tee -a "$LOG_FILE"
 else
-    echo "[$(date)] [WARN] docker.sock이 존재하지 않습니다. (/var/run/docker.sock)" | tee -a "$LOG_FILE"
+    echo "[$(date)] [WARN] docker.sock이 존재하지 않습니다." | tee -a "$LOG_FILE"
 fi
 
-touch /tmp/jenkins-entrypoint-test
+# Java 환경 설정
+export JAVA_HOME=/opt/java/openjdk21
+export PATH=$JAVA_HOME/bin:$PATH
+
 echo "[$(date)] [INFO] ENTRYPOINT 실행 완료" | tee -a "$LOG_FILE"
 
-# jenkins로 전환
-exec su - jenkins -c "/usr/bin/tini -- /usr/local/bin/jenkins.sh $*"
+# jenkins 사용자로 전환 + 환경 유지
+exec gosu jenkins /usr/local/bin/jenkins.sh "$@"
